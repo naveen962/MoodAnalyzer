45b63fc8de1e1ddffc3a97d28c4b4b928da68482
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-12 04:48:31 -0700

    program to check mood by passing null is added

---
+++ UC2MoodAnalyzer/UC2MoodAnalyzer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC2MoodAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To MoodAnalyzer");
+            MoodAnalyser mood = new MoodAnalyser(" ");
+            string res = mood.AnalyseMood();
+            Console.WriteLine(res);
+        }
+    }
+}
